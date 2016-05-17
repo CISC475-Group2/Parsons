3539232f4cb57a88a274ec3e64b619f1a79029d7
Keith Yong <terda12@gmail.com>
2016-05-17 13:22:22 -0400

    a

--- test.rkt
+++ test.rkt
@@ -1,4 +1,4 @@
 #! /usr/bin/env racket 
  #lang racket/base 
 (require test-engine/racket-tests)
-(define ( car-distance t) (* (/ 44 2.8) (* t t))) )(check-within 500 500 0.001)(test)
+(  2(- 3 3))(check-within 500 500 0.001)(test)
