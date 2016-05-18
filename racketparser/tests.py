f39ca7cf710491068bb4d609b2234115e67753d5
David Alexander <davidale@udel.edu>
2016-05-18 16:22:15 -0400

    parser fix

--- racketparser/tests.py
+++ racketparser/tests.py
@@ -18,11 +18,12 @@ class ParserTest(TestCase):
         self.assertEqual([['(', '<nt>', '<nt>', ')'], ['sqrt'], ['16']], generate_parson_question('(sqrt 16)'))
 
     def test_problem_generates_correct_blocks(self):
-        p1 = Problem(solution='(+ (- 3 2) (* 2 5))', compiles_to='11', points=5)
+        p1 = Problem(solution='(+ (- 3 2) (* 2 5))', evaluates_to='11', points=5)
         b1 = p1.generate_initial_data()['blocks']
         self.assertEqual(b1, [['(', '<nt>', '<nt>', '<nt>', ')'], ['+'], ['(', '<nt>', '<nt>', '<nt>', ')'], ['-'], ['3'], ['2'], ['(', '<nt>', '<nt>', '<nt>', ')'], ['*'], ['2'], ['5']])
 
-        p2 = Problem(solution='(sqrt 16)', compiles_to='11', points=5)
+        p2 = Problem(solution='(sqrt 16)', evaluates_to='11', points=5)
+        p2 = Problem(solution='(sqrt 16)', evaluates_to='11', points=5)
         b2 = p2.generate_initial_data()['blocks']
         self.assertEqual(b2, [['(', '<nt>', '<nt>', ')'], ['sqrt'], ['16']])
 
@@ -34,3 +35,4 @@ class ParserTest(TestCase):
         self.assertRaises(SyntaxError, parse, "( test( 1 2)))")
         self.assertRaises(SyntaxError, parse, "( test( 1 2)")
         self.assertRaises(SyntaxError, parse, ')')
+3
