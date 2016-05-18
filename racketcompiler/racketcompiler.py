07b9350debe83dc8f30ddb9b4794b4f9ce6d4907
David Alexander <davidale@udel.edu>
2016-05-18 16:21:22 -0400

    racketcompiler changes

--- racketcompiler/racketcompiler.py
+++ racketcompiler/racketcompiler.py
@@ -1,29 +1,32 @@
-#from django.db import Problem
+from app.models import  Problem
 import os.path
 import subprocess
 
-def compile_and_run(problem, userGeneratedResult, expectedTest ):
-    generateTestFile(problem, userGeneratedResult, expectedTest)
-    output = subprocess.check_output(["raco", "test" , "test.rkt" ])
-    removeTestFile()
-    if(str(output).find("passed")):
-        return ["passed",output]
-    return ["failed",output]
+def compile_and_run(problem, userGeneratedResult):
+    generateTestFile(problem, userGeneratedResult)
+    try:
+        output = subprocess.check_output(["raco", "test" , "test.rkt" ])
+        removeTestFile()
+        if(str(output).find("passed")):
+            return ["passed",output]
+        return ["failed",output]
+    except:
+        return ["failed","parse Error"]
 
-def generateTestFile(problem, userGeneratedResult, expectedTest):
+def generateTestFile(problem, userGeneratedResult):
     preface = '#! /usr/bin/env racket \n#lang racket/base \n(require test-engine/racket-tests)\n'
     suffix = '\n(test)'
+    expectedTest = problem.test_code
     #randgen for file name
     if os.path.isfile("test.rkt"):
         #file is in use sleep?
-        return
-    else:
-        file = open("test.rkt", "w")
-        file.write(preface)
-        file.write(userGeneratedResult)
-        file.write(expectedTest)
-        file.write(suffix)
-        file.close()
+        removeTestFile()
+    file = open("test.rkt", "w")
+    file.write(preface)
+    file.write(userGeneratedResult)
+    file.write(expectedTest)
+    file.write(suffix)
+    file.close()
 
 def removeTestFile():
     os.remove("test.rkt")
--- racketcompiler/tests.py
+++ racketcompiler/tests.py
@@ -4,6 +4,7 @@ from .racketcompiler import compile_and_run
 
 
 
-class ParserTest(TestCase):
-   def racket_problem_simulation(self):
-       print(compile_and_run("s", "(define ( car-distance t) (* (/ 44 2.8) (* t t))) ","(check-within 500 500 0.001)"))
+class CompilerTest(TestCase):
+   def test_racket_problem_simulation(self):
+        p = Problem(solution='(- 2 (+ 3 2))',test_code="(check-within 500 500 0.001)")
+        self.assertEqual(compile_and_run(p, "(define ( car-distance t) (* (/ 44 2.8) (* t t))) ")[0],"failed")
