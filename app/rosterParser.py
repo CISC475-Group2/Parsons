e94124965d88e7c8d2a302453168628b886c01bc
David Alexander <davidale@udel.edu>
2016-05-20 21:15:04 -0400

    resolving issues with class upload

--- app/rosterParser.py
+++ app/rosterParser.py
@@ -1,4 +1,5 @@
 from .models import  Student, Section
+from django.contrib.auth.models import User
 def parse_csv():
     """parse CSV and return elements in list
 
@@ -10,7 +11,6 @@ def parse_csv():
         for line in data:
             if len(line.split(',')) ==6:
                 studentList.append(line.split(','))
-                print(line.split(','))
 
        # data = line.split(',')
         #studentList.append(data)
@@ -26,6 +26,45 @@ def parse(file):
 
 def add_to_database(list):
     for item in list:
-        name= item[0]
-        section = item[4]
-       # s = Student.objects.create(user = name,section= section )
+        lastName = item[0]
+        firstName = item[1]
+
+        lastName = lastName[1:]
+        firstName = firstName[:-1]
+        firstName= firstName.lstrip(' ')
+        username = firstName[0]+lastName
+        print (firstName[0])
+        id = item[2]
+        password = id[0:4]
+        section= item[4]
+        if not (User.objects.filter(username=username).exists()):
+            #sectionList = item[4].split(",")
+
+            #for item in sectionList:
+            #   item=item.lstrip()
+
+            user = User.objects.create_user(
+                username = username,
+                email = "",
+                password = password,
+                first_name = firstName,
+                last_name = lastName
+            )
+            student = Student()
+
+            #for section in sectionList:
+            #   section = section.strip()
+            #  if not (Section.objects.get(section=section)):
+            #     Section.objects.create(section=section)
+            if not (Section.objects.filter(section = section).exists()):
+                section = Section.objects.create(section=section)
+            else:
+               section = Section.objects.get(section= section)
+
+
+            student.user = user
+            student.section = section
+            student.save()
+
+            user.student = student
+            user.save()
