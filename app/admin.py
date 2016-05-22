ef30536c1d74399bb8437a75c49f4b09cb1270a2
David Alexander <davidale@udel.edu>
2016-05-21 22:06:21 -0400

    add assigments to admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Problem, Solved, Section, Student
+from .models import Problem, Solved, Section, Student, Assignment
 
 admin.site.register(Problem)
 admin.site.register(Solved)
 admin.site.register(Section)
 admin.site.register(Student)
+admin.site.register(Assignment)
